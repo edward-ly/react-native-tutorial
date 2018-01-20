442c163699fcf8bfdf599ac77a7f59405164f7ad
Edward Ly <edvard.ly@gmail.com>
2018-01-20 15:25:04 -0800

    State

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowingText: true,
+    };
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ isShowingText: !this.state.isShowingText, });
+    }, 1000);
+  }
+
   render() {
-    return (
-      <Text>Hello {this.props.name}!</Text>
-    );
+    return <Text>{ this.state.isShowingText ? this.props.text : '' }</Text>;
   }
 }
 
-export default class LotsOfGreetings extends Component {
+export default class BlinkApp extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
