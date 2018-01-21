eace1de30d9fddcb33b25c9629b538b897c8d9ee
Edward Ly <edvard.ly@gmail.com>
2018-01-20 16:08:32 -0800

    Justify Content

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
+export default class JustifyContentBasics extends Component {
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
-      <View style={{ flex: 1, flexDirection: 'row' }}>
+      // Try setting 'justifyContent' to 'flex-start', 'center', 'flex-end', 'space-around', or 'space-between'.
+      // The 'space-evenly' value is invalid.
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+      }}>
         <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
         <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
         <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
