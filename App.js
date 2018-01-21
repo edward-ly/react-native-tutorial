4a4f8647107f0c56c91d16582800bf8ad4a57d67
Edward Ly <edvard.ly@gmail.com>
2018-01-20 21:42:16 -0800

    Refactored code to use FlatList over ListView, added PropTypes

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { ActivityIndicator, Alert, Button, FlatList, Image, ListView, Platform, ScrollView, SectionList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class Movies extends Component {
+  static propTypes = {
+    isLoading: PropTypes.bool,
+    dataSource: PropTypes.array,
+  };
+  
   constructor (props) {
     super(props);
     this.state = {
@@ -12,33 +18,22 @@ export default class Movies extends Component {
   componentDidMount () {
     return fetch('https://facebook.github.io/react-native/movies.json')
       .then( response => response.json() )
-      .then( responseJson => {
-        let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-        this.setState({
-          isLoading: false,
-          dataSource: ds.cloneWithRows(responseJson.movies),
-        }, () => {
-          // do something with new state
-        });
-      })
+      .then( json => this.setState({
+        isLoading: false,
+        dataSource: json.movies,
+      }))
       .catch( e => console.error(e) );
   }
 
   render () {
-    if (this.state.isLoading) {
-      return (
-        <View style={{ flex: 1, paddingTop: 20 }}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
     return (
       <View style={{ flex: 1, paddingTop: 20 }}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={ rowData => <Text>{rowData.title}, {rowData.releaseYear}</Text> }
-        />
+        { this.state.isLoading ?
+          <ActivityIndicator /> :
+          <FlatList data={this.state.dataSource}
+                    keyExtractor={ item => item.title }
+                    renderItem={ ({item}) => <Text>{item.title}, {item.releaseYear}</Text> } />
+        }
       </View>
     );
   }
